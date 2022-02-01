9dca28eb02ac76583ade16472b0088139c0cd289
bpocsics <bap106@uakron.edu>
2022-02-01 10:06:09 -0500

    created event loop docs

---
+++ 1-event-loop-examples/1-read-file.js
@@ -0,0 +1,14 @@
+const { readFile } = require('fs')
+
+console.log('started first task')
+// CHECK FILE PATH!!
+// reading the file
+readFile('./content/first.txt', 'utf-8', (err, result) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log(result)
+    console.log('completed first task')
+})
+console.log('starting next task')
---
+++ 1-event-loop-examples/3-setInterval.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+    console.log('hello world')
+}, 2000)
+console.log('I will run first')
+//process stays alive unless
+// kill proccess ctrl + c
+// unexpected error
