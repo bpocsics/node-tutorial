47abefb02028a2c34f542056c970c731d12c2eb5
bpocsics <bap106@uakron.edu>
2022-02-03 09:20:50 -0500

    more lessons added

--- 2-async-patterns/2-await-pattern.js
+++ 2-async-patterns/2-await-pattern.js
@@ -1,36 +1,42 @@
-const { readFile, WriteFile} = require('fs').promises
+const { readFile, writeFile } = require('fs').promises
 // const util = require('util')
 // const readFilePromise = util.promisify(readFile)
 // const writeFilePromise = util.promisify(writeFile)
 
-const start = async () => {
+
+
+const start = async() => {
     try {
         const first = await readFile('./content/first.txt', 'utf-8')
         const second = await readFile('./content/second.txt', 'utf-8')
         await writeFile(
-            './content/result-mind-grenade.txt',
-            `THIS IS AWESOME : ${first} ${second}`,
-            { flag: 'a' }
+            './content/result-mind-grenade.txt'
+            ,`THIS IS AWESOME : ${first} ${second}`,
+            { flag: 'a'}
         )
-    console.log(first, second)
-    } catch (error) {
+        console.log(first, second)
+    } catch (error){
         console.log(error)
     }
+
 }
 
 start()
 
+
+
+// getText('./content/second.txt')
+//     .then(result => console.log(result))
+//     .catch((err) => console.log(err))
+
 // const getText = (path) => {
-//   return new Promise((resolve, reject) => {
-//     readFile(path, 'utf8', (err, data) => {
-//       if (err) {
-//         reject(err)
-//       } else {
-//         resolve(data)
-//       }
+//     return new Promise((resolve,reject) => {
+//         readFile(path, 'utf-8', (err, data) => {
+//             if (err) {
+//                 reject(err)
+//             } else {
+//                 resolve(data)
+//             }
+//         })
 //     })
-//   })
 // }
-// getText('./content/first.txt')
-//   .then((result) => console.log(result))
-//   .catch((err) => console.log(err))
--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 const http = require('http')
 
+// const server = http.createServer((req, res) => {
+//   res.end('Welcome')
+// })
 
-const server = http.createServer((req,res) => {
-if(req.url === '/'){
-    res.end('Home Page')
-} else if(req.url === '/about'){
-    // BLOCKING CODE !!!!!!
-    for(let i = 0; i < 1000; i++){
-        for(let j = 0; j < 1000; j++){
-            console.log(`${i} ${j}`)
-        } 
-    }
-    res.end('About Page')
-} else {
-    res.end('Error Page')
-}
+// Using Event Emitter API
+const server = http.createServer()
+// emits request event
+// subcribe to it / listen for it / respond to it
+server.on('request', (req, res) => {
+  res.end('Welcome')
 })
 
-server.listen(5000, () => {
-    console.log('Server Listening on port 5000...')
-})
+server.listen(5000)
